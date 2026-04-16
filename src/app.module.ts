7015319801d6070c85a69a6c2e177aabd4604906
Kamal <kamal@matat.io>
2026-04-16 11:49:10 +0545

    feat(config): add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validateEnv } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnv,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
